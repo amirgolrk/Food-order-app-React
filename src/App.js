a7bc8d8f8b3e1035bed16d24f5f29970b4f56a7c
ahg80 <ah.golrokhi@gmail.com>
2023-05-14 20:29:37 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { React,Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Header />
+        <Meals />
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './MealsSummary.module.css'
+const MealsSummary = () =>{
+    return (
+        <section className={classes.summary}>
+            <h2>delicious food , delivered to you</h2>
+            <p>our foods are cooked with lots of love , dedication and resposibility to make you feel like your first taste</p>
+            <p>we are open from 11 am until 12 pm so you call us for your lunch and dinner and we can deliver it to you instanly and hot like it was in oven just few seconds ago . </p>
+        </section>
+    )
+}
+export default MealsSummary;
